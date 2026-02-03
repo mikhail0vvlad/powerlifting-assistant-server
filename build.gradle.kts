4f9a3769559611f6565024cc5ab73bb2f615f51d
Vlad Mikhailov <vlad.mikhailov.05@gmail.com>
2026-02-03 20:00:00 +0300

    chore: ktor 2.x project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    application
+}
+repositories { mavenCentral() }
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.7")
+    implementation("io.ktor:ktor-server-netty:2.3.7")
+}
+application { mainClass.set("com.powerlifting.server.MainKt") }
